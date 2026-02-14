589fb76b9e025e374a0bad6cd482dbe4b66ee43e
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2026-02-14 19:19:02 -0300

    chore(dependencies): reorganizar dependências e atualizar entrada do tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/ui/icons/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
-  external: ["react", "react-dom"],
+  external: ["react", "react-dom", "zustand"],
   tsconfig: "./tsconfig.build.json",
+  outDir: "dist",
 });
