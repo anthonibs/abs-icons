f297bb7bf9dc6d67962cf0e5d21a32775bf8f27e
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2026-02-16 01:24:22 -0300

    refactor(tsup): otimizar configuração de entrada e habilitar recursos de divisão e treeshake

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "tsup";
+import pkg from "glob";
+const { sync: globSync } = pkg;
+
+const iconFiles = globSync("src/ui/icons/**/*.{ts,tsx}");
 
 export default defineConfig({
-  entry: ["src/ui/icons/index.ts"],
+  entry: ["src/ui/icons/index.ts", ...iconFiles],
   format: ["esm", "cjs"],
   dts: true,
   clean: true,
-  sourcemap: false,
   minify: true,
+  splitting: true,
+  treeshake: true,
   external: ["react", "react-dom", "zustand"],
   tsconfig: "./tsconfig.build.json",
   outDir: "dist",
+  bundle: true,
 });
